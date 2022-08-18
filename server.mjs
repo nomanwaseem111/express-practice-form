c1225a0317af90fa334d405474b4c55f3d0c1efc
Noman Waseem <nomanwaseem111@gmail.com>
2022-08-18 18:01:30 +0500

    express server Practice

---
+++ server.mjs
@@ -0,0 +1,61 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(express.json())
+app.use(cors())
+const port = 3000
+
+let userBase = [];
+
+app.post('/signup', (req,res) => {
+
+    let body = req.body;
+
+    if(!body.firstname || !body.lastname || !body.email || !body.password ){
+
+        res.status(401).send(
+            `All Fields Required, For Example
+            
+             {
+                firstname : "noman",
+                lastname : "ahmed",
+                email : noman@abc.com,
+                password : "1234"
+             }
+            `)
+            return;
+        }
+
+        let isFound = false 
+
+
+        for(let i = 0; i < userBase.length; i++){
+
+            if(userBase[i].email === body.email){
+                isFound = true
+                break;
+            }
+        }
+
+        if(isFound){
+            res.status(401).send(
+                {message : "This Email id is Already Registered"})
+        }
+
+        let newUser = {
+
+            firstname : body.firstname,
+            lastname : body.lastname,
+            email : body.email.toLowerCase,
+            password : body.password
+        }
+
+        userBase.push(newUser);
+
+        res.status(201).send({message : "User is Created"});
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
